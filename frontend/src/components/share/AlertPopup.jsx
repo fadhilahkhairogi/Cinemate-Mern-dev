8b94ffb37b777583ec92cc84e6440dbc75e683d6
Xacolv <taufikrizki34@gmail.com>
2025-12-17 22:12:45 +0700

    Slicing UI Admin Pengguna

--- frontend/src/components/share/AlertPopup.jsx
+++ frontend/src/components/share/AlertPopup.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
-import { CheckCircle, XCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { CheckCircle, X, XCircle } from "lucide-react";
 
-function AlertPopup({ type = "success", message, onClose }) {
+function AlertPopup({ type = "success", message, onClose, duration = 2500 }) {
   const [isClosing, setIsClosing] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsClosing(true);
+      setTimeout(onClose, 300);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration, onClose]);
+
   return (
     <div
-      className={`fixed top-6 right-6 z-999 ${
-        isClosing ? "animate-slideOut" : "animate-slideIn"
-      }`}
+      className={`fixed top-6 right-6 z-999
+        ${isClosing ? "animate-slideOut" : "animate-slideIn"}
+      `}
     >
       <div
-        className={`flex items-center gap-3 px-5 py-4 rounded-xl shadow-lg text-white
-        ${type === "success" ? "bg-linear-to-r from-[#00FF1A] to-[#00df16]" : "bg-linear-to-r from-[#FF0004] to-[#b90003]"}`}
+        className={`flex items-center gap-1.5 px-5 py-4 rounded-xl shadow-lg text-white
+          ${
+            type === "success"
+              ? "bg-linear-to-r from-[#00FF1A] to-[#00df16]"
+              : "bg-linear-to-r from-[#FF0004] to-[#b90003]"
+          }
+        `}
       >
         {type === "success" ? (
           <CheckCircle size={22} />
@@ -26,13 +41,13 @@ function AlertPopup({ type = "success", message, onClose }) {
             setIsClosing(true);
             setTimeout(onClose, 300);
           }}
-          className="ml-3 text-white/80 hover:text-white"
+          className="text-white/80 hover:text-white"
         >
-          ✕
+          <X size={22} />
         </button>
       </div>
     </div>
   );
 }
 
-export default AlertPopup
+export default AlertPopup;
